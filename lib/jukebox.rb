bde4650630853d9cb50ae5d5412505e766b52706
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-08-01 03:54:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,9 @@ end
 
 def find_song(choice,songs)
   songs.each_with_index do |song,index|
-    if index == (choice - 1)
+    if index == (choice.to_i - 1)
+      return song 
+    elsif song == choice 
       return song 
     end 
   end 
@@ -29,8 +31,8 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  choice = gets.strip.to_i
-  if choice.between?(1,9)
+  choice = gets.strip 
+  if choice.to_i.between?(1,9) or choice == find_song(choice,songs)
     selected = find_song(choice,songs)
     puts "Playing #{selected}"
   else 

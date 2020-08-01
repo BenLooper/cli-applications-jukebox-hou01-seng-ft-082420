9c5a7c566730c9e24a689353190274530194d245
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-08-01 03:12:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,3 +3,9 @@ def say_hello(name)
   "Hi #{name}"
 end 
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you chooe a song to play"
+  puts "- exit : exits this program"
